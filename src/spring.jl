9bd088827c85b176bb09348ecc2cdb1a6c3dde5a
ywang552 <ywang552@syr.edu>
2025-01-16 15:57:31 -0500

    x

--- src/spring.jl
+++ src/spring.jl
@@ -1,23 +1,24 @@
 using DifferentialEquations, Optim, Plots
 ts = reshape(ts, 21, 5, 10)
-pt[:,10]
-
+tsode = reshape(tsode, 21, 5, 10)
 # Observed data
 t_obs = 1:11  # Time steps 11 to 21
-X_obs = tsode[11:21,1,1]
+X_obs = ts[1:11,5,1]
+
+pt
 
 # Spring model (2nd-order differential equation)
 function spring!(du, u, p, t)
     X, dX = u
     β, ω0, X_steady = p
     du[1] = dX
-    du[2] = -β * dX - ω0^2 * (X - X_steady)
+    du[2] = - β * dX - ω0^2 * (X - X_steady)
 end
 
 # Loss function to optimize
 function loss(params)
     u0 = [X_obs[1], 0.0]  # Initial conditions: [X(0), dX(0)]
-    tspan = (0.0, 10.0)
+    tspan = (0.0, 20.0)
     prob = ODEProblem(spring!, u0, tspan, params)
     sol = solve(prob, saveat=1.0)
     X_pred = [sol[i][1] for i in 1:length(t_obs)]
@@ -43,60 +44,19 @@ sol = solve(prob, saveat=1.0)
 X_pred = [sol[i][1] for i in 1:length(t_obs)]
 
 # Plot observed vs predicted
-p = plot(t_obs .+ 10, X_obs, label="Observed", marker=:circle, lw = 4)
+p = plot(t_obs .+ 10, X_obs, label="Observed", marker=:circle, lw = 4, ylims = [0, 1])
 plot!(t_obs .+ 10, X_pred, label="solver (RK4)", linestyle=:dash, lw = 4)
 xlabel!("Time")
 ylabel!("Gene 1 Expression")
 
-# Optimized parameters (replace with actual values)
-β_opt = β_opt             # Replace with the optimized β
-ω0_opt = ω0_opt            # Replace with the optimized ω0
-X_steady_opt = X_steady_opt      # Replace with the optimized X_steady
-
-# Initial conditions
-x_0 = X_obs[1]          # Initial position (from observed data)
-v_0 = 0.0               # Initial velocity
-Δt = 0.1                # Time step size
-num_steps = 100         # Number of steps to predict
 
 
 
 
 
-# # Initial conditions
-# x_0 = X_obs[1]          # Initial position (from observed data)
-# v_0 = 0.0               # Initial velocity
-# Δt = 0.1                # Time step size
-# num_steps = 100         # Number of steps to predict
 
 
-# function predict_motion_rk4(x_0, v_0, β, ω0, X_steady, Δt, num_steps)
-#     # Initialize arrays to store results
-#     x_values = [x_0]
-#     v_values = [v_0]
 
-#     # Initialize current position and velocity
-#     x_t = x_0
-#     v_t = v_0
-
-#     # Predict step-by-step
-#     for step in 1:num_steps
-#         x_t, v_t = predict_next_step_rk4(x_t, v_t, β, ω0, X_steady, Δt)
-#         push!(x_values, x_t)
-#         push!(v_values, v_t)
-#     end
-
-#     # Generate time points
-#     t_values = (0:num_steps) .* Δt
-
-#     return t_values, x_values, v_values
-# end
-
-# # Call the function to predict motion
-# t_values, x_values, v_values = predict_motion_rk4(x_0, v_0, β_opt, ω0_opt, X_steady_opt, Δt, num_steps)
-
-# plot!(t_values.+10, x_values, label="RK4", xlabel="Time", ylabel="gene value", lw =4)
-# savefig("figs\\g1(unregulated)_SHM.png")
 
 display(p)
 
--- src/spring_regulated.jl
+++ src/spring_regulated.jl
@@ -1,26 +1,28 @@
 using DifferentialEquations, Optim, Plots
 # Observed time-series data
 t_obs = 1:11  # Time steps
+
 ts = reshape(ts, 21, 5, 10)
 
 tsode = reshape(tsode, 21, 5, 10)
 tssde = reshape(tssde, 21, 5, 10)
-pt
-g = 1
+
+
+g = 4
 tri = 1
+println(gs[:,g])
 plot(ts[1:end,tri,g])
 plot!(tsode[1:end,tri,g])
 plot!(tssde[1:end,tri,g])
-X_obs_2 = tsode[11:21, tri, g]  # Observed data for g2
-X_inputs = tsode[11:21, tri, findall(x->x!=0,gs[:,g])]  # Observed data for g1, g6, g8
+X_obs_2 = tsode[11:21, tri, g]  
+X_inputs = tsode[11:21, tri, findall(x->x!=0,gs[:,g])] 
 
-# Define ODE for g2 dynamics
 
 function regulated_spring!(du, u, p, t)
     X2, dX2 = u  # Current state of g2
     β, ω0, X_steady_2, W... = p  # Parameters
     du[1] = dX2
-    du[2] = -β * dX2 - ω0^2 * (X2 - X_steady_2) + X_inputs[Int(t),:]' * W
+    du[2] = -β * dX2 - ω0^2 * (X2 - X_steady_2) + X_inputs[Int(floor(t))+1,:]' * W 
 end
 
 function regulated_repression!(du, u, p, t)
@@ -54,9 +56,9 @@ end
 function loss_spring(params)
     β, ω0, X_steady_2, W... = params
     u0 = [X_obs_2[1], 0.0]
-    tspan = (1, 11)
-    prob = DiscreteProblem(regulated_spring!, u0, tspan, [β, ω0, X_steady_2, W...])
-    sol = solve(prob, saveat=1)
+    tspan = (0., 10.)
+    prob = ODEProblem(regulated_spring!, u0, tspan, [β, ω0, X_steady_2, W...])
+    sol = solve(prob, saveat=1.)
     X_pred = [sol[i][1] for i in 1:length(X_obs_2)]
     return sqrt(mean((X_obs_2 .- X_pred).^2))
 end
@@ -73,7 +75,7 @@ optimized_params
 # Solve the spring equation with optimized parameters
 u0 = [X_obs_2[1], 0.0]
 tspan = (0, 10)
-prob = DiscreteProblem(regulated_spring!, u0, tspan, optimized_params)
+prob = ODEProblem(regulated_spring!, u0, tspan, optimized_params)
 sol = solve(prob, saveat=1.0)
 
 
@@ -82,7 +84,7 @@ X_pred = [sol[i][1] for i in 1:length(t_obs)]
 X_obs_2
 
 
-p = plot(t_obs .+ 10, X_obs_2, label="Observed", marker=:circle, lw = 4)
+p = plot(t_obs .+ 10, X_obs_2, label="Observed", marker=:circle, lw = 4, ylims = [0, 1])
 plot!(t_obs .+ 10, X_pred, label="solver (RK4)", linestyle=:dash, lw = 4)
 xlabel!("Time")
 ylabel!("Gene 1 Expression")
